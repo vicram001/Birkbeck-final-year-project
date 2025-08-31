62f9e03007bf54dbfccc90968d169b893c954c6e
Vic <25566126+vicram001@users.noreply.github.com>
2025-08-31 14:03:05 +0100

    Update tokenize_with_spacy_train_save_evaluate_csv_save.py

--- tokenize_with_spacy_train_save_evaluate_csv_save.py
+++ tokenize_with_spacy_train_save_evaluate_csv_save.py
@@ -44,7 +44,7 @@ train_data = [
 split = int(0.8 * len(train_data))
 train_data, test_data = train_data[:split], train_data[split:]
 
-# create blank model with English tokeniser / pipeline
+# create blank model with English tokenizer / pipeline
 nlp = spacy.blank("en")
 
 # add TextCategoriser to the pipeline with default config
